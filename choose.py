362741e7c88764c4e04389dc43414bbd48f9c13c
Harlin Seritt <harlin.seritt@gmail.com>
2022-03-31 19:20:02 -0600

    Auto push to repo at 31Mar22-192001

--- choose.py
+++ choose.py
@@ -11,17 +11,37 @@ debug = True
 target_text = ''
 discarded_text = ''
 
-try:
-    target_text = open(target_file, 'r').read()
-except FileNotFoundError as e:
-    open(target_file, 'w').close()
-    target_text = open(target_file, 'r').read()
+def get_target_text():
+    try:
+        target_text = open(target_file, 'r').read()
+    except FileNotFoundError as e:
+        open(target_file, 'w').close()
+        target_text = open(target_file, 'r').read()
+    return target_file
 
-try:
-    discarded_text = open(discarded_file, 'r').read()
-except FileNotFoundError as e:
-    open(discarded_file, 'w').close()
-    discarded_text = open(discarded_file, 'r').read()
+
+def get_discarded_text():
+    try:
+        discarded_text = open(discarded_file, 'r').read()
+    except FileNotFoundError as e:
+        open(discarded_file, 'w').close()
+        discarded_text = open(discarded_file, 'r').read()
+    return discarded_text
+
+
+def get_answer():
+    if debug:
+        print('-- start --')
+    answer = input('\ntext: {}\n\n(Y)es/(N)o/E(x)it > '.format(line))
+    if debug:
+        print('-- end --\n\n')
+    return answer
+
+
+
+target_text = get_target_text()
+discarded_text = get_discarded_text()
+
 
 for line in open(source_file, 'r').readlines():
     line = line.rstrip()
@@ -29,11 +49,7 @@ for line in open(source_file, 'r').readlines():
         if line != '' and not line.endswith(':'):
             unanswered = True
             while unanswered:
-                if debug:
-                    print('-- start --')
-                answer = input('\ntext: {}\n\n(Y)es/(N)o/E(x)it > '.format(line))
-                if debug:
-                    print('-- end --\n\n')
+                answer = get_answer()
 
                 if answer.lower() == 'y':
                     print('Saving line to {}'.format(target_file))
